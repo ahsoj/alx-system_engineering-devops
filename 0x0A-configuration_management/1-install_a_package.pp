2c739fecc19c172f2d673b4b6cd112c92248962e
eyasu Chaka <daa282386@gmail.com>
2023-03-24 04:33:12 -0700

    puppet

--- 0x0A-configuration_management/1-install_a_package.pp
+++ 0x0A-configuration_management/1-install_a_package.pp
@@ -1,5 +1,6 @@
-# Using Puppet, install flask from pip3.
+#Using Puppet, install flask from pip3.
 
-exec { 'flask':
+exec { 'flask installation':
     command => 'pip3 install -Iv flask==2.1.0',
+    path => ['/usr/bin/'],
 }
