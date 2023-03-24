0b74d54efe6b94699af5ad90feaf3305daea048f
eyasu Chaka <daa282386@gmail.com>
2023-03-24 04:14:53 -0700

    puppet

--- 0x0A-configuration_management/0-create_a_file.pp
+++ 0x0A-configuration_management/0-create_a_file.pp
@@ -1,6 +1,8 @@
+#Using Puppet, create a file in /tmp.
+
 file {'/tmp/school/':
     mode    => '0744',
     owner   => 'www-data',
     group   => 'www-data',
-    content => 'I love Puppet'
+    content => 'I love Puppet',
 }
