894cd74278bd81892dfe9ea78b8399565ecda266
eyasu chaka <daa282386@gmail.com>
2023-04-24 10:12:42 -0700

    save_it_all

--- 0x15-api/3-dictionary_of_list_of_dictionaries.py
+++ 0x15-api/3-dictionary_of_list_of_dictionaries.py
@@ -12,14 +12,22 @@ def main():
     __url_2 = requests.get(
                   'https://jsonplaceholder.typicode.com/users').json()
 
-    with open("todo_all_employees.json", 'w') as jsonFile:
-        for i in range(len(__url)):
-            username = __url_2[int(__url[i]['userId']) - 1]['username']
-            json.dump({__url[i]['userId']: [{
-                        "username": username,
-                        "task": __url[i]['title'],
-                        "completed": __url[i]['completed'],
-                        }]}, jsonFile)
+    __objects = {}
+    for user in __url_2:
+        user_id = user.get('id')
+        user_name = user.get('username')
+        todos = list(filter(lambda x: x.get('userId') == user_id, __url))
+        __object = list(map(
+            lambda x: {
+                'username': user_name,
+                'task': x.get('title'),
+                'completed': x.get('completed')
+            },
+            todos
+        ))
+        __objects[user_id] = __object
+    with open('todo_all_employees.json', 'w') as jsonFile:
+        json.dump(__objects, jsonFile, sort_keys=False)
 
 
 if __name__ == "__main__":
