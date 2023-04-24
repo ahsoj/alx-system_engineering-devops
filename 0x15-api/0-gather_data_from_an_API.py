5c42aedab445f4f021041d5bcf08d1c07643ac92
eyasu chaka <daa282386@gmail.com>
2023-04-24 06:32:20 -0700

    task_filter

--- 0x15-api/0-gather_data_from_an_API.py
+++ 0x15-api/0-gather_data_from_an_API.py
@@ -4,15 +4,14 @@
 import requests
 
 
-if __name__ == "__main__":
-    import sys
-    _id = sys.argv[1]
+def main(arg):
+    """request todos and retrieve data"""
     __url = requests.get(
              'https://jsonplaceholder.typicode.com/todos/?userId={id}'.format(
-                                 id=_id)).json()
+                                 id=arg)).json()
     __url_2 = requests.get(
                   'https://jsonplaceholder.typicode.com/users/{id}'.format(
-                                  id=_id)).json()
+                                  id=arg)).json()
     n_done = 0
     n_task = len(__url)
     for i in range(len(__url)):
@@ -22,3 +21,8 @@ if __name__ == "__main__":
     print(pron.format(e_name=__url_2['name'], n_done=n_done, n_task=n_task))
     for i in range(len(__url)):
         print("\t {}".format(__url[i]['title']))
+
+
+if __name__ == "__main__":
+    from sys import argv
+    main(argv[1])
