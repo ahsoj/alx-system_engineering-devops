d9dae978dc80d9a284366b1508bae62f893eecd3
eyasu chaka <daa282386@gmail.com>
2023-05-09 06:55:14 -0700

    api_advanced

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -5,4 +5,9 @@ import requests
 
 def number_of_subscribers(subreddit):
     """"return number of subscribers or None"""
-    
+    URL = "https://www.reddit.com/r/{}/about/.json".format(subreddit)
+    response = requests.get(URL)
+    if response.status_code == 200:
+        response = response.json()
+        return (response['data']['subscribers'])
+    return 0
