e540e5d9c30efbd07333a87ddce2c5bb15e88558
eyasu chaka <daa282386@gmail.com>
2023-05-10 05:59:29 -0700

    web_stack_debugging_3

---
+++ 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
@@ -0,0 +1,6 @@
+# 0-strace_is_your_friend.pp file must contain Puppet code
+
+exec { 'fix-wordpress':
+  command => 'sed -i s/phpp/php/g /var/www/html/wp-settings.php',
+  path    => '/usr/local/bin/:/bin/'
+}
---
+++ 0x17-web_stack_debugging_3/README.md
@@ -0,0 +1,2 @@
+# 0x17. Web stack debugging #3
+![](https://s3.amazonaws.com/intranet-projects-files/holbertonschool-sysadmin_devops/293/d42WuBh.png)
