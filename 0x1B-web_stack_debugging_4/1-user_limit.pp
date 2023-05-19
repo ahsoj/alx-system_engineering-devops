1434ba6cc877ec2a0928c70b1f4f4cabc1ba755f
eyasu chaka <daa282386@gmail.com>
2023-05-19 02:28:53 -0700

    web_stack_webugging

---
+++ 0x1B-web_stack_debugging_4/1-user_limit.pp
@@ -0,0 +1,11 @@
+# change the os configuration
+
+exec { 'increase-hard_file-limit':
+  command => 'sed -i "/holberton hard/s/5/50000/" /etc/security/limits.conf',
+  path    => '/usr/local/bin/:/bin/'
+}
+
+exec { 'increase-soft_file-limit':
+  command => 'sed -i "/holberton soft/s/4/50000/" /etc/security/limits.conf',
+  path    => '/usr/local/bin/:/bin/'
+}
